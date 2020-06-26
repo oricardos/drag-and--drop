d842bd3087baae5bf184a379677321a1d8f9f933
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-06-26 00:08:27 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,57 @@
+function log(message) {
+    console.log(message)
+}
+
+const cards = document.querySelectorAll('.card')
+const dropzones = document.querySelectorAll('.dropzone')
+
+cards.forEach(card => {
+    card.addEventListener('dragstart', dragstart)
+    card.addEventListener('drag', drag)
+    card.addEventListener('dragend', dragend)
+})
+
+function dragstart() {
+    dropzones.forEach ( dropzone => dropzone.classList.add('highlight'))
+    
+    this.classList.add('is-dragging')
+}
+
+function drag() {
+    log('during')
+}
+
+function dragend() {
+    dropzones.forEach ( dropzone => dropzone.classList.remove('highlight'))
+
+    this.classList.remove('is-dragging')
+
+}
+
+dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+function dragenter() {
+    log('enter')
+}
+
+function dragover() {
+    this.classList.add('over')
+
+    // get draggin card
+    const cardBeingDragged = document.querySelector('.is-dragging')
+
+    this.appendChild(cardBeingDragged)
+}
+
+function dragleave() {
+    this.classList.remove('over')
+}
+
+function drop() {
+    log('drop')
+}
