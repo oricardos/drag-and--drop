3d913b3ce9272e9a402aebc1256f018b3eca6347
oricardos <ricardo.rs95@gmail.com>
2022-05-05 17:08:25 -0300

    feat(geral): add novas funcionalidades

--- modules/createLink.js
+++ modules/createLink.js
@@ -1,11 +1,13 @@
 export default function createLink(el) {
-  const linkValue = document.getElementById('beacons-links').value;
+  const linkName = document.getElementById('beacons-link-name').value;
+  const linkValue = document.getElementById('beacons-link-value').value;
+
   if (linkValue !== '') {
     const link = document.createElement('a');
     link.classList.add('link-primary');
     link.setAttribute('href', linkValue);
     link.setAttribute('target', '_blank');
-    link.innerHTML = linkValue;
+    link.innerHTML = linkName;
     el.appendChild(link);
   }
 }
