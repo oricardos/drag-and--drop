34aa27c67f92583ca867379760c57eb014a71e58
oricardos <ricardo.rs95@gmail.com>
2022-05-09 14:38:48 -0300

    feat(draganddrop): separando drag

--- modules/create/createProfilePhoto.js
+++ modules/create/createProfilePhoto.js
@@ -1,19 +1,28 @@
-export default function createProfilePhoto(el) {
-  const photo = document.getElementById('beacons-profile-photo').value;
-  if (photo) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+import createSectionEl from './createSectionEl.js';
 
-    const col = document.createElement('div');
-    col.classList.add('col-12');
+export default function createProfilePhoto() {
+  const photo = document.getElementById('beacons-profile-photo');
+  const addProfilePhoto = document.querySelector('.add-profilephoto');
 
-    //CRIANDO IMAGEM
-    const img = document.createElement('img');
-    img.classList.add('img-fluid', 'rounded');
-    img.src = photo;
-    col.appendChild(img);
+  addProfilePhoto.addEventListener('click', function (evt) {
+    if (photo.files[0].name !== '') {
+      const el = evt.target.parentElement;
+      const section = el.parentElement.classList[1];
 
-    row.appendChild(col);
-    el.appendChild(row);
-  }
+      const row = document.createElement('div');
+      row.classList.add('row');
+
+      const col = document.createElement('div');
+      col.classList.add('col-12');
+
+      //CRIANDO IMAGEM
+      const img = document.createElement('img');
+      img.classList.add('img-fluid', 'rounded');
+      img.src = photo.files[0].name;
+      col.appendChild(img);
+
+      row.appendChild(col);
+      createSectionEl(section, row);
+    }
+  });
 }
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import cookie from './modules/cookie.js';
+import draganddrop from './modules/draganddrop.js';
+import createProfilePhoto from './modules/create/createProfilePhoto.js';
 import createText from './modules/create/createText.js';
 import createLink from './modules/create/createLink.js';
 import createEmail from './modules/create/createEmail.js';
@@ -7,62 +9,11 @@ import createYouTube from './modules/create/createYouTube.js';
 import createMusic from './modules/create/createMusic.js';
 
 cookie();
+draganddrop();
+createProfilePhoto();
 createText();
 createLink();
 createEmail();
 createTweet();
 createYouTube();
 createMusic();
-
-function log(message) {
-  console.log(message);
-}
-
-const cards = document.querySelectorAll('.card-element');
-const dropzones = document.querySelectorAll('[class^="dropzone-"]');
-
-cards.forEach((card) => {
-  card.addEventListener('dragstart', dragstart);
-  card.addEventListener('drag', drag);
-  card.addEventListener('dragend', dragend);
-});
-
-function dragstart() {
-  dropzones.forEach((dropzone) => dropzone.classList.add('highlight'));
-  this.classList.add('is-dragging');
-}
-
-function drag() {
-  log('during');
-}
-
-function dragend() {
-  dropzones.forEach((dropzone) => dropzone.classList.remove('highlight'));
-  this.classList.remove('is-dragging');
-}
-
-dropzones.forEach((dropzone) => {
-  dropzone.addEventListener('dragenter', dragenter);
-  dropzone.addEventListener('dragover', dragover);
-  dropzone.addEventListener('dragleave', dragleave);
-  dropzone.addEventListener('drop', drop);
-});
-
-function dragenter() {
-  log('enter');
-}
-
-function dragover() {
-  this.classList.add('over');
-  // get draggin card
-  const cardBeingDragged = document.querySelector('.is-dragging');
-  this.appendChild(cardBeingDragged);
-}
-
-function dragleave() {
-  this.classList.remove('over');
-}
-
-function drop() {
-  log('drop');
-}
