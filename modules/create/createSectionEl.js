c2a27fb1ef19f033c444045479a36d89b2bf1adc
oricardos <ricardo.rs95@gmail.com>
2022-05-13 14:12:39 -0300

    style(preview): melhorando interface do preview

--- modules/create/createSectionEl.js
+++ modules/create/createSectionEl.js
@@ -1,6 +1,6 @@
 export default function createSectionEl(section, el) {
   const divSection = document.createElement('div');
-  const preview = document.querySelector('.dropzone-preview');
+  const preview = document.querySelector('.preview');
 
   divSection.classList.add(`beacons-${section}`);
   divSection.appendChild(el);
--- modules/create/createYouTube.js
+++ modules/create/createYouTube.js
@@ -6,25 +6,25 @@ export default function createYouTube() {
   const video = window.localStorage.getItem('video');
 
   // função que cria o elemento
-  function createYoutubeSection(link){
-      const row = document.createElement('div');
-      const col = document.createElement('div');
-      const titleSection = document.createElement('h3');
-      
-      row.classList.add('row');
-      col.classList.add('video-container');
-      titleSection.innerHTML = 'Youtube';
-      titleSection.classList.add('p-0');
+  function createYoutubeSection(link) {
+    const row = document.createElement('div');
+    const col = document.createElement('div');
+    const titleSection = document.createElement('h3');
 
-      const video = `
+    row.classList.add('row');
+    col.classList.add('video-container');
+    titleSection.innerHTML = 'Youtube';
+    titleSection.classList.add('p-0');
+
+    const video = `
     <iframe width="560" height="315" src="https://www.youtube.com/embed/${link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     `;
 
-      col.innerHTML = video;
-      row.appendChild(titleSection);
-      row.appendChild(col);
+    col.innerHTML = video;
+    row.appendChild(titleSection);
+    row.appendChild(col);
 
-      createSectionEl('section-video', row);
+    createSectionEl('section-video', row);
   }
 
   addLink.addEventListener('click', function () {
@@ -32,13 +32,14 @@ export default function createYouTube() {
       window.localStorage.setItem('video', linkValue.value);
 
       createYoutubeSection(linkValue.value);
+      document.location.reload(true);
     }
   });
 
   if (video || linkValue.value !== '') {
-      const switchYouTube = document.querySelector('.switch-video');
-      switchYouTube.classList.remove('d-none');
-      switchYouTube.classList.add('d-flex');
+    const switchYouTube = document.querySelector('.switch-video');
+    switchYouTube.classList.remove('d-none');
+    switchYouTube.classList.add('d-flex');
 
     linkValue.value = video;
     createYoutubeSection(video);
