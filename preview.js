54eb2d325aec1c6335d3fbb4fb13f485f02606fc
oricardos <ricardo.rs95@gmail.com>
2022-05-30 15:11:00 -0300

    feat(preview): mostrando username

--- preview.js
+++ preview.js
@@ -1,3 +1,5 @@
 import previewProfilePhoto from './modules/preview/previewProfilePhoto.js';
+import previewUsername from './modules/preview/previewUsername.js';
 
 previewProfilePhoto();
+previewUsername();
