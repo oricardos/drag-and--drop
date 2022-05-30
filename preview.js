fe01c4d1654a1f7ae2b976be94cf71ebfaea6f3b
oricardos <ricardo.rs95@gmail.com>
2022-05-30 15:37:32 -0300

    feat(preview): mostrando bio

--- preview.js
+++ preview.js
@@ -1,5 +1,7 @@
 import previewProfilePhoto from './modules/preview/previewProfilePhoto.js';
 import previewUsername from './modules/preview/previewUsername.js';
+import previewBio from './modules/preview/previewBio.js';
 
 previewProfilePhoto();
 previewUsername();
+previewBio();
