521bdc29ea722df71ced459c98adbf5b5a5aeb88
oricardos <ricardo.rs95@gmail.com>
2022-06-02 16:48:16 -0300

    ajustes pequenos

--- js/modules/intro.js
+++ js/modules/intro.js
@@ -2,22 +2,22 @@ export default function intro() {
 introJs().setOptions({
     "dontShowAgain": true,
     steps:[{
-        title: 'Seja bem-vindo ao Beacons!',
+        title: 'Seja bem-vindo ao Beacons! 👋',
         intro: 'Seja bem-vindo ao Beacons da deep web'
     },{
-        title: 'Conteúdo',
+        title: '📝 Conteúdo',
         element: document.querySelector('a.intro-content'),
-        intro: 'Na aba CONTEÚDO você consegue gerenciar todo o conteúdo da sua página.'
+        intro: 'Na aba <b>Conteúdo</b> você consegue gerenciar todo o conteúdo da sua página.'
     },{
-        title: 'Aparência',
+        title: '🖌 Aparência',
         element: document.querySelector('a.intro-appearance'),
         intro: 'Na aba APARÊNCIA você consegue personalizar sua página e deixar ela com a sua cara.'
     },{
-        title: 'Preview',
+        title: 'Preview 👀',
         element: document.querySelector('.intro-preview'),
         intro: 'Na área de preview, você pode ver como está ficando o seu projeto.'
     },{
-        title: 'Vamos Começar!',
+        title: 'Vamos Começar! 🏁',
         element: document.querySelector('.intro-start'),
         intro: 'Comece adicionando uma foto sua...'
     }]
