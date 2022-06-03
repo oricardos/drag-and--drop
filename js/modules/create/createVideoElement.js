4962c3583f5a13bd5fb73465251d6cb1b523aa4b
oricardos <ricardo.rs95@gmail.com>
2022-06-03 09:59:57 -0300

    fix(video): correcao do src dos videos

--- js/modules/create/createVideoElement.js
+++ js/modules/create/createVideoElement.js
@@ -1,27 +1,31 @@
 export default function createVideoElement(link) {
+  console.log(link);
   const plataforms = ['tiktok', 'vimeo', 'youtube', 'instagram'];
   const plataformLink = link.split('/')[2].split('.');
   let plataform = plataforms.filter((p) => plataformLink.includes(p));
   let h3;
   let className;
   let id;
-  let src = `https://${plataform}/${id}`;
+  let src;
 
   if (plataform[0] === 'youtube') {
     h3 = 'YouTube';
     className = 'youtube';
     plataform = 'youtube.com/embed';
     id = link.split('v=')[1].split('&')[0];
+    src = `https://${plataform}/${id}`;
   } else if (plataform[0] === 'vimeo') {
     h3 = 'Vimeo';
     className = 'vimeo';
     plataform = 'player.vimeo.com/video';
     id = link.split('/')[link.split('/').length - 1];
+    src = `https://${plataform}/${id}`;
   } else if (plataform[0] === 'tiktok') {
     h3 = 'TikTok';
     className = 'tiktok';
     plataform = 'tiktok.com/embed';
     id = link.split('/')[5].split('id=');
+    src = `https://${plataform}/${id}`;
   } else if (plataform[0] === 'instagram') {
     h3 = 'Instagram';
     className = 'instagram';
@@ -32,7 +36,9 @@ export default function createVideoElement(link) {
 
   const video = `
   <h3>${h3}</h3>
+  <div>
     <iframe title="url" class="${className}Wrapper" src="${src}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
     `;
 
   return video;
